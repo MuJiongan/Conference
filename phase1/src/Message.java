ee961b283238d27eec54ccf84370717c75605788
Xinyi Huang <xcindy.huang@mail.utoronto.ca>
2020-10-25 14:48:36 -0400

    Add an Entity class called Message.

---
+++ phase1/src/Message.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+
+/**
+ * Represents the messages for the conference.
+ */
+public class Message {
+
+    /**
+     * Stores the User id of sender
+     */
+    private int senderID;
+
+    /**
+     * Stores the User id of receiver
+     */
+    private int receiverID;
+
+    /**
+     * Stores the message content
+     */
+    private String content;
+
+    /**
+     * Stores the total number of Messages
+     */
+    private static int numOfMessages = 0;
+
+    /**
+     * Stores the id of the message
+     */
+    private int messageID;
+
+
+    /**
+     * Constructor:
+     *   Create a new Message object given
+     *   @param _senderID of the message,
+     *   @param _receiverID of the message,
+     *   @param _content of the message.
+     */
+    public Message(int _senderID, int _receiverID, String _content){
+        this.senderID = _senderID;
+        this.receiverID = _receiverID;
+        this.content = _content;
+        this.messageID = numOfMessages;
+        numOfMessages++;
+    }
+
+    /**
+     * Getter:
+     *    Returns the senderID of message
+     */
+    public int getSenderID(){
+        return senderID;
+    }
+
+    /**
+     * Getter:
+     *    Returns the receiverID of message
+     */
+    public int getReceiverID(){
+        return receiverID;
+    }
+
+    /**
+     * Getter:
+     *    Returns the content of message
+     */
+    public String getContent(){
+        return content;
+    }
+
+    /**
+     * Getter:
+     *    Returns the messageID of message
+     */
+    public int getMessageID() {
+        return messageID;
+    }
+
+}
