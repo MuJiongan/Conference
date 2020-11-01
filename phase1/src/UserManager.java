856e40faba930c97913be24ee8f01c5428175cce
Jiongan Mu <jiongan.mu@mail.utoronto.ca>
2020-11-01 17:47:33 -0500

    Initiated all the lists in UserManager.java and implemented serializable

--- phase1/src/UserManager.java
+++ phase1/src/UserManager.java
@@ -1,18 +1,19 @@
+import java.io.*;
 import java.util.ArrayList;
 
-public class UserManager {
+public class UserManager implements Serializable{
     /**
      * The list of all attendees
      */
-    private ArrayList<Attendee> attendees;
+    private ArrayList<Attendee> attendees = new ArrayList<Attendee>();
     /**
      * The list of all organizers
      */
-    private ArrayList<Organizer> organizers;
+    private ArrayList<Organizer> organizers = new ArrayList<Organizer>();
     /**
      * The list of all speakers
      */
-    private ArrayList<Speaker> speakers;
+    private ArrayList<Speaker> speakers = new ArrayList<Speaker>();
     /**
      * Returns the shallow copy of all attendees in a list
      * @return the shallow copy of all attendees in a list
@@ -183,4 +184,42 @@ public class UserManager {
     public ArrayList<Integer> getEventList(User user){
         return user.getEventsAttend();
     }
+    /**
+     * Read the UserManager object that was stored in a .ser file
+     * @param path String representing the file path
+     * @return UserManager object read from .ser file
+     * @throws ClassNotFoundException is thrown if UserManager object is not found
+     */
+    public UserManager readFromFile (String path) throws ClassNotFoundException {
+
+        try {
+            InputStream file = new FileInputStream(path); // String path should be "fileName.ser"
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+
+            // deserialize the StudentManager
+            UserManager um = (UserManager) input.readObject();
+            input.close();
+            return um;
+        } catch (IOException ex) {
+            return new UserManager();
+        }
+    }
+
+    /**
+     * Write the UserManager object to a .ser file to store once program exists
+     * @param filePath file to write to
+     * @throws IOException is thrown if file we want to write to does not exist
+     */
+    public void saveToFile(String filePath) throws IOException {
+
+        OutputStream file = new FileOutputStream(filePath);
+        OutputStream buffer = new BufferedOutputStream(file);
+        ObjectOutput output = new ObjectOutputStream(buffer);
+
+        // serialize the UserManager
+        output.writeObject(this);
+        output.close();
+    }
+
 }
