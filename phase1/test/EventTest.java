750dd0825d41622b872658513c1081bb5a02cb18
syxnsa <syxnsa@gmail.com>
2020-11-06 01:45:09 +0800

    modify EventTest

--- phase1/test/EventTest.java
+++ phase1/test/EventTest.java
@@ -31,12 +31,27 @@ public class EventTest {
         assertSame("incorrect StartTime\n", start1, speak1.getStartTime());
 
     }
+    //test getEndTime
     @Test(timeout = 50)
     public void testGetEndTime() {
         LocalDateTime start1 = LocalDateTime.of(2020, 1, 1, 20, 0, 0);
         LocalDateTime end1 = LocalDateTime.of(2020, 1, 1, 21, 0, 0);
         Event speak1 = new Event(start1, end1, 101, "CSC207", 30);
         assertSame("incorrect StartTime\n", end1, speak1.getEndTime());
+    }
+
+    //test setStartTime and setEndTIme
+    @Test(timeout = 50)
+    public void testSetterOfTime(){
+        LocalDateTime start1 = LocalDateTime.of(2020, 1, 1, 20, 0, 0);
+        LocalDateTime start2 = LocalDateTime.of(2020, 2, 1, 9, 0, 0);
+        LocalDateTime end1 = LocalDateTime.of(2020, 1, 1, 21, 0, 0);
+        LocalDateTime end2 = LocalDateTime.of(2020, 2, 1, 10, 0, 0);
+        Event speak1 = new Event(start1, end1, 101, "CSC207", 30);
+        speak1.setStartTime(start2);
+        speak1.setEndTime(end2);
+        assertSame("incorrect StartTime\n", start2, speak1.getStartTime());
+        assertSame("incorrect EndTime\n", end2, speak1.getEndTime());
     }
 
     @Test(timeout = 50)
@@ -56,8 +71,8 @@ public class EventTest {
         speak1.addUserID(321);
         speak1.addUserID(1234);
         speak1.removeUserID(321);
-        assertEquals("incorrect UserID",Integer.valueOf(123), speak1.getUserIDs().get(0) );
-        assertEquals("incorrect UserID",Integer.valueOf(1234), speak1.getUserIDs().get(1) );
+        assertEquals("incorrect UserID\n",Integer.valueOf(123), speak1.getUserIDs().get(0) );
+        assertEquals("incorrect UserID\n",Integer.valueOf(1234), speak1.getUserIDs().get(1) );
     }
 
     @Test(timeout = 50)
@@ -69,8 +84,8 @@ public class EventTest {
         speak1.addSpeakerID(4567);
         speak1.addSpeakerID(23355);
         speak1.removeSpeakerID(4567);
-        assertEquals("incorrect SpeakerID", Integer.valueOf(567), speak1.getSpeakerIDs().get(0));
-        assertEquals("incorrect SpeakerID", Integer.valueOf(23355), speak1.getSpeakerIDs().get(1));
+        assertEquals("incorrect SpeakerID\n", Integer.valueOf(567), speak1.getSpeakerIDs().get(0));
+        assertEquals("incorrect SpeakerID\n", Integer.valueOf(23355), speak1.getSpeakerIDs().get(1));
     }
 
     @Test(timeout = 50)
@@ -78,21 +93,10 @@ public class EventTest {
         Event speak1 = new Event(LocalDateTime.of(2020, 1, 1, 20, 0, 0),
                 LocalDateTime.of(2020, 1, 1, 21, 0, 0), 101,
                 "CSC207", 30);
-        assertSame("incorrect RoomID", 101, speak1.getRoomID());
+        assertSame("incorrect RoomID\n", 101, speak1.getRoomID());
         speak1.changeRoomID(102);
-        assertSame("incorrect RoomID", 102, speak1.getRoomID());
+        assertSame("incorrect RoomID\n", 102, speak1.getRoomID());
     }
 
-    @Test(timeout = 50)
-    public void testGetEventID() {
-        Event speak1 = new Event(LocalDateTime.of(2020, 1, 1, 20, 0, 0),
-                LocalDateTime.of(2020, 1, 1, 21, 0, 0), 101,
-                "CSC207", 30);
-        Event speak2 = new Event(LocalDateTime.of(2020, 1, 1, 10, 0, 0),
-                LocalDateTime.of(2020, 1, 1, 11, 0, 0), 121,
-                "CSC258", 40);
-        assertSame("incorrect EventID", 0, speak1.getEventID());
-        assertSame("incorrect EventID", 1, speak2.getEventID());
-    }
 
 }
