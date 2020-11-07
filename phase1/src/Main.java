6f948435292634bce10590df4f5fa3809c05a6ce
Jonathan <jc09310@gmail.com>
2020-11-07 16:03:24 -0500

    Commented out hardcoded accounts

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -5,19 +5,19 @@ import java.util.ArrayList;
 public class Main
 {
     public static void main(String[] args){
-        Organizer o = new Organizer("Jonathan", "chenjo14", "12345678");
-        Speaker s = new Speaker("Steve", "ste123", "steven");
-        OrganizerManager om = new OrganizerManager();
-        SpeakerManager sm = new SpeakerManager();
-        om.addUser(o);
-        sm.addUser(s);
-        try{
-            om.saveToFile("phase1/src/organizermanager.ser");
-            sm.saveToFile("phase1/src/speakermanager.ser");
-        }catch(IOException i )
-        {
-            System.out.println(i.getMessage());
-        }
+//        Organizer o = new Organizer("Jonathan", "chenjo14", "12345678");
+//        Speaker s = new Speaker("Steve", "ste123", "steven");
+//        OrganizerManager om = new OrganizerManager();
+//        SpeakerManager sm = new SpeakerManager();
+//        om.addUser(o);
+//        sm.addUser(s);
+//        try{
+//            om.saveToFile("phase1/src/organizermanager.ser");
+//            sm.saveToFile("phase1/src/speakermanager.ser");
+//        }catch(IOException i )
+//        {
+//            System.out.println(i.getMessage());
+//        }
 
         ConferenceSystem c = new ConferenceSystem();
         c.run();
