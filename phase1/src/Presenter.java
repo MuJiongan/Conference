299f5af22b4ed6c75ab776b34fbbaa2ce769c3bc
YANLING-H <67870437+YANLING-H@users.noreply.github.com>
2020-11-10 16:21:31 -0500

    small change to presenter

--- phase1/src/Presenter.java
+++ phase1/src/Presenter.java
@@ -49,47 +49,44 @@ public class Presenter {
         }
 
 
+
         public static void viewMyEvents (ArrayList < Event > eventsTheyAttended, EventManager em){
             String divider = "------------------------";
             String heading1 = "Events";
             String heading2 = "Time";
+            String heading3 = "Room";
             int i = 1;
-    public static void viewMyEvents(ArrayList<Event> eventsTheyAttended, EventManager em){
-        String divider = "------------------------";
-        String heading1 = "Events";
-        String heading2 = "Time";
-        String heading3 = "Room";
-        int i = 1;
 
-        System.out.println("Here is your events:");
-        System.out.println("");
-        System.out.printf("%-15s %-15s %-15s %n", heading1, heading2, heading3);
-        for (Event event: eventsTheyAttended){
-            System.out.printf("%-15s %10s %n", i + "." + em.getName(event),em.getStartTime(event) + " "
-                    + em.getEndTime(event));
-            i += 1;
+            System.out.println("Here is your events:");
+            System.out.println("");
+            System.out.printf("%-15s %-15s %-15s %n", heading1, heading2, heading3);
+            for (Event event : eventsTheyAttended) {
+                System.out.printf("%-15s %10s %n", i + "." + em.getName(event), em.getStartTime(event) + " "
+                        + em.getEndTime(event));
+                i += 1;
+            }
+            System.out.println(divider);
         }
-        System.out.println(divider);
-    }
 
-    public static void viewAllEvents(ArrayList<Event> allEventsInSystem, EventManager em){
-        String divider = "------------------------";
-        String heading1 = "Events";
-        String heading2 = "Time";
-        String heading3 = "Vacancy";
-        String heading4 = "Room";
-        int i = 1;
+            public static void viewAllEvents (ArrayList < Event > allEventsInSystem, EventManager em){
+                String divider = "------------------------";
+                String heading1 = "Events";
+                String heading2 = "Time";
+                String heading3 = "Vacancy";
+                String heading4 = "Room";
+                int i = 1;
 
-        System.out.println("Here is all the scheduled events:");
-        System.out.println("");
-        System.out.printf("%-15s %-15s %-15s %-15s %n", heading1, heading2, heading3, heading4);
-        for (Event event: allEventsInSystem){
-            System.out.printf("%-15s %-10s %d %n", i + "." + em.getName(event),em.getStartTime(event) + " "
-                    + em.getEndTime(event) + " ", em.getVacancy(event));
-            i += 1;
+                System.out.println("Here is all the scheduled events:");
+                System.out.println("");
+                System.out.printf("%-15s %-15s %-15s %-15s %n", heading1, heading2, heading3, heading4);
+                for (Event event : allEventsInSystem) {
+                    System.out.printf("%-15s %-10s %d %n", i + "." + em.getName(event), em.getStartTime(event) + " "
+                            + em.getEndTime(event) + " ", em.getVacancy(event));
+                    i += 1;
+                }
+                System.out.println(divider);
+            }
         }
-        System.out.println(divider);
-    }
 
 //        String display = "Here is your schedule:";
 //        int i = 1;
