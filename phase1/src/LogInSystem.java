a38394c31ca3f2e846b7de990fae2bc920928a6b
Jiongan Mu <jiongan.mu@mail.utoronto.ca>
2020-11-11 18:48:03 -0500

    added initializeAttendeeContactsList

--- phase1/src/LogInSystem.java
+++ phase1/src/LogInSystem.java
@@ -89,6 +89,7 @@ public class LogInSystem implements UserController {
                 {
                     Attendee newAccount = am.createAttendee(inputs.get(0), inputs.get(1), inputs.get(2), getNewID());
                     am.addUser(newAccount);
+                    initializeAttendeeContactsList(newAccount);
                     return newAccount;
                 }
             }
